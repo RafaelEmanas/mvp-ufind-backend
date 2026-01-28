40eb33a63d92299f39911ceb1abc6c05c5b64b50
RafaelEmanas <rafael.emanueldv.pro@gmail.com>
2026-01-28 16:22:26 -0400

    First Commit

---
+++ src/main/java/com/ufind/ufindapp/UfindappApplication.java
@@ -0,0 +1,13 @@
+package com.ufind.ufindapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfindappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfindappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufind/ufindapp/UfindappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufind.ufindapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfindappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
