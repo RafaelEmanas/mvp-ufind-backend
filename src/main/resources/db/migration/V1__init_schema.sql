7a9cec7ab4d1c66c312eaa311718d31cb5cea3fc
RafaelEmanas <rafael.emanueldv.pro@gmail.com>
2026-01-31 17:54:08 -0400

    add: Define flyway migrations

---
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -0,0 +1,22 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS items (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title VARCHAR(200) NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    date_found DATE NOT NULL,
+    location_found VARCHAR(255) NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    image_url VARCHAR(500),
+    contact_info VARCHAR(255),
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
+);
---
+++ src/main/resources/db/migration/V4__add_admin.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO users (username, email, password, role)
+VALUES (
+    'Rafael Emanuel',
+    'rafael.emanueldv.pro@gmail.com',
+    '$2a$10$jdFUQWBfhowAOyaiItXKPugckNvFxaI3R.A7o9omySYTqAJwY6f4e',
+    'ADMIN'
+);
